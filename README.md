8ddebcc02f31fc30a54fc86beacc19e330336293
Yang Zhou <deedecho@gmail.com>
2020-05-23 23:59:13 +0800

    Add nats apis

--- README.md
+++ README.md
@@ -1,14 +1,61 @@
 ## tea-layer1-facade
 
-### listen new block
-Example:
+#### Start
 
-subscribe the follow subject:
+Run nats service
 ```
-layer1.chain.newheader
+docker pull nats
+docker run -p 4222:4222 -ti nats:latest
 ```
 
-response block height and hash:
+Run layer1
+
+Run layer1 facade
 ```
-9183.0xc3a9fce3fc02df21a8d2efc045b438227e1fd8a6e48d0a512e144852df0fa86f
+npm install
+npm start
+```
+
+#### Nats Api
+> Use nats-box an example of nats.rs for testing
+
+- Get node info
+```bash
+cargo run --example nats-box -- sub 'layer1_reply.*.>'
+cargo run --example nats-box -- pub layer1.async.layer1_reply.node_info eb628d56ad353cc7a9b4db31aae999c402a02da9da6d2651a8e9aa2f73920b95
+```
+
+Response:
+```
+Received a Message {
+  subject: "layer1_reply.action.node_info",
+  data: "{"key":"0xeb628d56ad353cc7a9b4db31aae999c402a02da9da6d2651a8e9aa2f73920b95","amt":321}"
+}
+```
+
+- Get bootstrap
+```bash
+cargo run --example nats-box -- sub 'layer1_reply.*.>'
+cargo run --example nats-box -- pub layer1.async.layer1_reply.bootstrap 123
+```
+
+Response:
+```
+Received a Message {
+  subject: "layer1_reply.action.bootstrap",
+  data: "["tea-node1","tea-node2"]"
+}
+```
+
+- Listen new block
+```bash
+cargo run --example nats-box -- sub 'layer1.chain.newheader'
+```
+
+Response:
+```
+Received a Message {
+  subject: "layer1.chain.newheader",
+  data: "11504.0x23491f576dd33543a4bc7fa2b99f922a44fc26e086adcfd0acda1b54df0028ee"
+}
 ```
--- index.js
+++ index.js
@@ -1,3 +1,65 @@
-const nats = require("./nats")
+const NATS = require('nats')
+const { ApiPromise } = require('@polkadot/api')
 
-nats.start()
+const nc = NATS.connect()
+
+function handle_new_header(header) {
+      nc.publish('layer1.chain.newheader', `${header.number}.${header.hash}`)
+}
+
+async function main() {
+      const api = await ApiPromise.create({
+            types: {
+                  Node: {
+                        "key": "Bytes",
+                        "amt": "u64"
+                  },
+                  Model: {
+                        "account": "AccountId",
+                        "payment": "u32",
+                        "cid": "H256"
+                  },
+                  Task: {
+                        "account": "H256",
+                        "amt": "Balance",
+                        "model_id": "Hash",
+                        "cid": "Hash"
+                  },
+                  Competitor: {
+                        "account": "H256",
+                        "task_id": "Hash",
+                        "random_value": "H256"
+                  }
+            }
+      })
+
+      // listen new block
+      api.rpc.chain.subscribeNewHeads((header) => {
+            // console.log(`chain is at #${header.number} has hash ${header.hash}`)
+            handle_new_header(header)
+      })
+
+      nc.subscribe('layer1.async.*.>', async function (msg, reply, subject, sid) {
+            // console.log('Received a message: ', msg, reply, subject, sid)
+            const subSections = subject.split('.')
+            // console.log(subSections)
+            const replyTo = subSections[2]
+            const action = subSections[3]
+            switch(action) {
+                  case 'bootstrap':
+                        nc.publish(replyTo + '.action.bootstrap', JSON.stringify(['tea-node1', 'tea-node2']))
+                        break;
+                  case 'node_info':
+                        const nodeInfo = await api.query.tea.nodes(msg);
+                        nc.publish(replyTo + '.action.node_info', JSON.stringify(nodeInfo))
+                        break;
+                  default:
+                        nc.publish(replyTo + '.error.action_does_not_support', '')
+            }
+      })
+}
+
+main().catch((error) => {
+      console.error(error)
+      process.exit(-1)
+})
