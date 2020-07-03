52c8870b9520d30d0b3faf7b2a0ce8cad8145a7d
Yang Zhou <deedecho@gmail.com>
2020-07-03 12:01:26 +0800

    update sample

--- sample/nats-pub.js
+++ sample/nats-pub.js
@@ -6,20 +6,20 @@ const proto = require('../src/proto');
 const nc = NATS.connect();
 
 function add_new_node() {
-      nc.publish('layer1.async.replay.add_new_node', '0x02b40e313842e45574e0ca5b37cb0580cc3378ceb096b562a9828b2137b98f5f', 'layer1.test.result')
+      nc.publish('layer1.async.reply.add_new_node', '0x02b40e313842e45574e0ca5b37cb0580cc3378ceb096b562a9828b2137b98f5f', 'layer1.test.result')
 }
 
 function update_peer_id() {
-      nc.publish('layer1.async.replay.update_peer_id', '0x02b40e313842e45574e0ca5b37cb0580cc3378ceb096b562a9828b2137b98f5f__0x1fbb8d02600f4931fbed2e4f998d9e16d1a95e6d4586b5787310a95d2f8a6ed4_0xa555a7e72e9810dde46ca653d56956a2d6e88bb3896038f19674bd3b02d94d18', 'layer1.test.result')
+      nc.publish('layer1.async.reply.update_peer_id', '0x02b40e313842e45574e0ca5b37cb0580cc3378ceb096b562a9828b2137b98f5f__0x1fbb8d02600f4931fbed2e4f998d9e16d1a95e6d4586b5787310a95d2f8a6ed4_0xa555a7e72e9810dde46ca653d56956a2d6e88bb3896038f19674bd3b02d94d18', 'layer1.test.result')
 }
 
 function get_nodes() {
-      nc.publish('layer1.async.replay.get_nodes', '', 'layer1.test.result')
+      nc.publish('layer1.async.reply.get_nodes', '', 'layer1.test.result')
 }
 
 function add_new_task() {
       const task = {
-            refNum: Buffer.from('03', 'hex'),
+            refNum: Buffer.from('01', 'hex'),
             delegateId: Buffer.from('01', 'hex'),
             modelCid: '444',
             bodyCid: '555',
@@ -35,7 +35,7 @@ function add_new_task() {
       // const newTask = newTaskBuf.decode(protoMsg);
       // console.log('3', newTask);
 
-      nc.publish('layer1.async.replay.add_new_task', taskBufBase64, 'layer1.test.result')
+      nc.publish('layer1.async.reply.add_new_task', taskBufBase64, 'layer1.test.result')
 }
 
 function complete_task() {
@@ -56,15 +56,15 @@ function complete_task() {
       // const newRequest = newRequestBuf.decode(Buffer.from(requestBase64, 'base64'));
       // console.log('decode:', newRequest);
 
-      nc.publish('layer1.async.replay.complete_task', requestBase64, 'layer1.test.result')
+      nc.publish('layer1.async.reply.complete_task', requestBase64, 'layer1.test.result')
 }
 
 async function main() {
       // add_new_node()
       // update_peer_id()
       // get_nodes()
-      // add_new_task()
-      complete_task()
+      add_new_task()
+      // complete_task()
 }
 
 main().catch((error) => {
--- sample/nats-sub.js
+++ sample/nats-sub.js
@@ -9,7 +9,7 @@ function main() {
       //       console.log('Received a message: ', msg, reply, subject, sid)
       // })
 
-      nc.subscribe('layer1.test.*.>', (msg, reply, subject, sid) => {
+      nc.subscribe('layer1.event.*.>', (msg, reply, subject, sid) => {
             console.log('Received a message: ', msg, reply, subject, sid)
       })
 }
