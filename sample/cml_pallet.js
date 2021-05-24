b094059d6e1fa79550006650d1733b7c2b47051f
jacky.li <jacky.li@accenture.com>
2021-05-24 13:49:44 +0800

    update for new layer1 with version3.0.0

--- sample/cml_pallet.js
+++ sample/cml_pallet.js
@@ -7,30 +7,30 @@ runSample(null, async (layer1)=>{
   const bob = layer1.getAccountFrom('Bob');
   // console.log(11, api.query.assets);
 
-  let alice_dai = await api.query.assets.daiStore(alice.address);
+  let alice_dai = await api.query.cml.daiStore(alice.address);
   console.log('alice dai =>', alice_dai.toHuman());
-  let bob_dai = await api.query.assets.daiStore(bob.address);
+  let bob_dai = await api.query.cml.daiStore(bob.address);
   console.log('bob dai =>', bob_dai.toHuman());
 
   // transfer 100 dai from alice to bob
   console.log('transfer 10 dai from alice to bob');
-  const tx = api.tx.assets.transferDai(bob.address, 10);
+  const tx = api.tx.cml.transferDai(bob.address, 10);
   await layer1.sendTx(alice, tx);
 
-  alice_dai = await api.query.assets.daiStore(alice.address);
+  alice_dai = await api.query.cml.daiStore(alice.address);
   console.log('alice dai =>', alice_dai.toHuman());
-  bob_dai = await api.query.assets.daiStore(bob.address);
+  bob_dai = await api.query.cml.daiStore(bob.address);
   console.log('bob dai =>', bob_dai.toHuman());
 
-  let alice_cml = await api.query.assets.cmlStore(alice.address);
+  let alice_cml = await api.query.cml.cmlStore(alice.address);
   console.log('alice cml =>', alice_cml.toHuman());
 
   // convert 1 dai to cml
-  const convert_tx = api.tx.assets.convertCmlFromDai();
+  const convert_tx = api.tx.cml.convertCmlFromDai();
   await layer1.sendTx(alice, convert_tx);
 
-  alice_cml = await api.query.assets.cmlStore(alice.address);
+  alice_cml = await api.query.cml.cmlStore(alice.address);
   console.log('alice cml =>', alice_cml.toHuman());
-  alice_dai = await api.query.assets.daiStore(alice.address);
+  alice_dai = await api.query.cml.daiStore(alice.address);
   console.log('alice dai =>', alice_dai.toHuman());
 });
